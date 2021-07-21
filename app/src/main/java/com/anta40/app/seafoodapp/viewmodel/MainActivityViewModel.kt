f04634eb305e3340d40313fad054675ad0cf1a0f
Andre Tampubolon <andre@lc.vlsm.org>
2021-07-21 09:22:49 +0700

    Useless line removed

--- app/src/main/java/com/anta40/app/seafoodapp/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/anta40/app/seafoodapp/viewmodel/MainActivityViewModel.kt
@@ -4,11 +4,13 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.anta40.app.seafoodapp.model.MealList
 import com.anta40.app.seafoodapp.repository.MainActivityRepository
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlinx.coroutines.launch
 
 class MainActivityViewModel : ViewModel() {
 
@@ -16,18 +18,19 @@ class MainActivityViewModel : ViewModel() {
     val seafoodListLiveData: LiveData<MealList > = _seafoodListLiveData
 
     fun getData(){
-        MainActivityRepository.getSeafoodListApiCall().enqueue(object: Callback<MealList> {
-            override fun onResponse(
-                call: Call<MealList>,
-                response: Response<MealList>
-            ) {
-                val body = response.body()
-                _seafoodListLiveData.value = response.body()
-            }
+       // viewModelScope.launch {
+            MainActivityRepository.getSeafoodListApiCall().enqueue(object : Callback<MealList> {
+                override fun onResponse(
+                    call: Call<MealList>,
+                    response: Response<MealList>
+                ) {
+                    val body = response.body()
+                }
 
-            override fun onFailure(call: Call<MealList>, t: Throwable) {
-                Log.e("MainViewModel", "onFailure: ", t)
-            }
-        })
+                override fun onFailure(call: Call<MealList>, t: Throwable) {
+                    Log.e("MainViewModel", "onFailure: ", t)
+                }
+            })
+        //}
     }
 }
